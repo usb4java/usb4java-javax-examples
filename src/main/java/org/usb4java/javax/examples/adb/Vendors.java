5dba373309e87f5bb27d11f9c3a0bb8b3d4945bc
Malcolm Crum <crummynz@gmail.com>
2016-01-07 11:14:17 +0100

    close parenthesis

--- src/main/java/org/usb4java/javax/examples/adb/Vendors.java
+++ src/main/java/org/usb4java/javax/examples/adb/Vendors.java
@@ -172,7 +172,7 @@ public class Vendors
 
     /**
      * The effective list of ADB USB vendor IDs (Including the ones read from
-     * the adb_usb.ini file.
+     * the adb_usb.ini file.)
      */
     public static short[] VENDOR_IDS = createVendorIds();
 
